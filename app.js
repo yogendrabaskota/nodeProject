be95cd9d9f857a1455478f3ccdbdce2e219c481e
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-05-30 14:58:50 +0545

    adding css

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.set('view engine','ejs')
 
 app.get('/', (req,res) => {
     res.render('home'); 
+})
 
-
+app.get('/home', (req,res) => {
+    res.render('home')
 })
 
 app.get('/register', (req,res) => {
