d5fd87b8c21d2adf1659fcd08aa526c1e68efdd0
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-05-31 23:03:05 +0545

    blog model added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 require("./model/index");
 
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true})) // to get form data of own frontend of nodejs
+// app.use(express.json)  // to get form data from reactjs(external frontend) 
+
 
 app.get('/', (req,res) => {
     res.render('home'); 
@@ -12,17 +15,21 @@ app.get('/', (req,res) => {
 app.get('/home', (req,res) => {
     res.render('home')
 })
-
 app.get('/register', (req,res) => {
     res.render('auth/register')
 })
-
-
-
 app.get('/login', (req,res) => {
     res.render('auth/login')
 })
 
+
+app.post('/register',(req,res) => {
+
+//    console.log(req.body)
+    const {username,email, password} = req.body
+
+
+})
 
 app.use(express.static('public/css'))
 
